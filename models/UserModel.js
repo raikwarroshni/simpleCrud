9acc685b4bf7c343d939b636ac081562352f4730
raikwarroshni <roshniraikwar97@gmail.com>
2022-02-17 18:19:23 +0530

    changes in valodations

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,20 +1,36 @@
 const {Schema, model} = require('mongoose');
+var validator = require('validator');
 
 const userModel = new Schema ({
   firstName: {
-    type: String
+    type: String,
+    trim:true
   },
   lastName: {
-    type: String
+    type: String,
+    trim:true
   },
   email: {
-    type: String
+    type: String,
+    required:true,
+    unique:true,
+    lowercase:true,
+    validate(value){
+   if(validator.isEmail(value)){
+     throw new error("emai; is invalid")
+   }
+    }
   },
   password: {
     type: String
   },
   otp: {
-    type: String
+    type: String,
+    validate(value){
+      if(value<0){
+        throw new error ("otp can not be null")
+      }
+    }
   },
   token: {
     type: String
